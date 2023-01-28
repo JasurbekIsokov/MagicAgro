f04f7cc8165a483c702533196c044feba3716e9e
Jasurbek <jasurisoqov685@gmail.com>
2023-01-29 01:58:12 +0500

    News section

--- src/components/Nevs/Nevs.jsx
+++ src/components/Nevs/Nevs.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useState, useEffect } from "react";
 
 import Slider from "react-slick";
+import useWindowDimensions from "../../Hooks/GetWindow";
 
 import {
   Section,
@@ -10,22 +11,129 @@ import {
 } from "../../styles/GlobalComponents";
 
 const Nevs = () => {
-  let settings = {
+  const { height, width } = useWindowDimensions();
+
+  useEffect(() => {
+    // console.log(width);
+  });
+
+  let sliderNumber = 3;
+
+  if (width <= 1050) {
+    sliderNumber = 2;
+  }
+
+  if (width <= 600) {
+    sliderNumber = 1;
+  }
+
+  const settings = {
     dots: true,
     infinite: true,
-    speed: 500,
-    slidesToShow: 1,
+    slidesToShow: sliderNumber,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
-    pauseOnHover: true,
+    rtl: true,
   };
 
   return (
     <Section nopadding id="nevs">
       <SectionTitle main>Yangiliklar</SectionTitle>
+
+      <div className="nevs">
+        <Slider {...settings}>
+          <div className="nevs__card">
+            <div className="nevs__card--box">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Consectetur ipsum veritatis, ullam optio dolor doloribus
+                corrupti et illo. Cumque, deleniti. saLorem ipsum dolor sit
+                amet, consectetur adipisicing elit. Consectetur ipsum veritatis,
+                ullam optio dolor doloribus corrupti et illo. Cumque,
+                deleniti.lom saLorem ipsum dolor sit amet, consectetur
+                adipisicing elit. Consectetur ipsum veritatis, ullam optio dolor
+                doloribus corrupti et illo. Cumque, deleniti.lom
+              </p>
+              <button>Read More</button>
+            </div>
+          </div>
+          <div className="nevs__card">
+            <div className="nevs__card--box">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Consectetur ipsum veritatis, ullam optio dolor doloribus
+                corrupti et illo. Cumque, deleniti. saLorem ipsum dolor sit
+                amet, consectetur adipisicing elit. Consectetur ipsum veritatis,
+                ullam optio dolor doloribus corrupti et illo. Cumque,
+                deleniti.lom saLorem ipsum dolor sit amet, consectetur
+                adipisicing elit. Consectetur ipsum veritatis, ullam optio dolor
+                doloribus corrupti et illo. Cumque, deleniti.lom
+              </p>
+              <button>Read More</button>
+            </div>
+          </div>
+          <div className="nevs__card">
+            <div className="nevs__card--box">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Consectetur ipsum veritatis, ullam optio dolor doloribus
+                corrupti et illo. Cumque, deleniti. saLorem ipsum dolor sit
+                amet, consectetur adipisicing elit. Consectetur ipsum veritatis,
+                ullam optio dolor doloribus corrupti et illo. Cumque,
+                deleniti.lom saLorem ipsum dolor sit amet, consectetur
+                adipisicing elit. Consectetur ipsum veritatis, ullam optio dolor
+                doloribus corrupti et illo. Cumque, deleniti.lom
+              </p>
+              <button>Read More</button>
+            </div>
+          </div>
+          <div className="nevs__card">
+            <div className="nevs__card--box">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Consectetur ipsum veritatis, ullam optio dolor doloribus
+                corrupti et illo. Cumque, deleniti. saLorem ipsum dolor sit
+                amet, consectetur adipisicing elit. Consectetur ipsum veritatis,
+                ullam optio dolor doloribus corrupti et illo. Cumque,
+                deleniti.lom saLorem ipsum dolor sit amet, consectetur
+                adipisicing elit. Consectetur ipsum veritatis, ullam optio dolor
+                doloribus corrupti et illo. Cumque, deleniti.lom
+              </p>
+              <button>Read More</button>
+            </div>
+          </div>
+          <div className="nevs__card">
+            <div className="nevs__card--box">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Consectetur ipsum veritatis, ullam optio dolor doloribus
+                corrupti et illo. Cumque, deleniti. saLorem ipsum dolor sit
+                amet, consectetur adipisicing elit. Consectetur ipsum veritatis,
+                ullam optio dolor doloribus corrupti et illo. Cumque,
+                deleniti.lom saLorem ipsum dolor sit amet, consectetur
+                adipisicing elit. Consectetur ipsum veritatis, ullam optio dolor
+                doloribus corrupti et illo. Cumque, deleniti.lom
+              </p>
+              <button>Read More</button>
+            </div>
+          </div>
+          <div className="nevs__card">
+            <div className="nevs__card--box">
+              <p>
+                saLorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Consectetur ipsum veritatis, ullam optio dolor doloribus
+                corrupti et illo. Cumque, deleniti.lom saLorem ipsum dolor sit
+                amet, consectetur adipisicing elit. Consectetur ipsum veritatis,
+                ullam optio dolor doloribus corrupti et illo. Cumque,
+                deleniti.lom
+              </p>
+              <button>Read More</button>
+            </div>
+          </div>
+        </Slider>
+      </div>
       <SectionDivider />
-      <div className="nevs"></div>
     </Section>
   );
 };
